c21a890e6d3bc76bbb69394e7242513007fdc046
Kosukey <koshuke0217@icloud.com>
2018-05-18 10:54:51 +0900

    java valuable scope

---
+++ if_check.java
@@ -0,0 +1,9 @@
+class if_check {
+    public static void main(String[] args) {
+        int i = 1;
+        if (i == 1){
+          i = 2;
+        }
+        System.out.println(i); //2が出力される
+    }
+}
